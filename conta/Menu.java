0f1d30bcf849165c3df90db4f2cbfa47d9e31157
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2023-11-16 13:49:12 -0300

    versão 1.0

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,10 +1,93 @@
 package conta;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner read = new Scanner(System.in);
+		
+		int option;
+		
+		while(true) {
+			
+			System.out.println("*********************************************************");
+			System.out.println("														 ");
+			System.out.println("		    BANCO DO BRAZIL  				  		 ");
+			System.out.println("														 ");
+			System.out.println("*********************************************************");
+			System.out.println("														 ");
+			System.out.println("	1 - Criar Conta  				  		 ");
+			System.out.println("	2 - Listar todas as Contas 				 ");
+			System.out.println("	3 - Buscar Conta por Número				 ");
+			System.out.println("	4 - Atualizar Dados da Conta			 ");
+			System.out.println("	5 - Apagar Conta			             ");
+			System.out.println("	6 - Sacar		                         ");
+			System.out.println("	7 - Depositar			              	 ");
+			System.out.println("	8 - Transferir Valores entre Contas		 ");
+			System.out.println("	9 - Sair		                     	 ");
+			System.out.println("														 ");
+			System.out.println("*********************************************************");
+			System.out.println("Entre com a opcao desejada:                              ");
+			System.out.println("														 ");
+			
+			option = read.nextInt();
+			
+			if(option == 9) {
+				System.out.println("\n Banco do Brazil. Seu futuro começa aqui!");
+				sobre();
+				read.close();
+				System.exit(0);
+			}
+			
+			switch(option) {
+			case 1: 
+				System.out.println("Criar Conta \n\n");
+			
+				break;
+			case 2: 
+				System.out.println("Listar todas as Contas \n\n");
+			
+				break;
+			case 3: 
+				System.out.println("Consultar dados da Conta - por número \n\n");
+			
+				break;
+			case 4: 
+				System.out.println("Atualizar dados da Conta \n\n");
+			
+				break;
+			case 5: 
+				System.out.println("Apagar a Conta \n\n");
+			
+				break;
+			case 6: 
+				System.out.println("Saque \n\n");
+			
+				break;
+			case 7: 
+				System.out.println("Depósito \n\n");
+			
+				break;
+			case 8: 
+				System.out.println("Transferência entre Contas \n\n");
+			
+				break;
+			default: 
+				System.out.println("Opção Inválida \n");
+			
+				break;
+			}
+		}
 
+	}
+	
+	public static void sobre() {
+			System.out.println("\n*********************************************************");
+			System.out.println("	Projeto Desenvolvido por: Rodrigo Schort     ");
+			System.out.println("	Email:  rodrigoschort01@gmail.com"            );
+			System.out.println("	Github: https://github.com/Cyberdrick"        );
 	}
 
 }
