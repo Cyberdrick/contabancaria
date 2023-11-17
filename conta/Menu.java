cc9b8356841ff1c3eae68ee224c89ff1987f3ace
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2023-11-17 13:42:57 -0300

    constructor

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,12 +1,23 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
 		Scanner read = new Scanner(System.in);
+		
+		//Teste da Classe Conta
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 		
 		int option;
 		
